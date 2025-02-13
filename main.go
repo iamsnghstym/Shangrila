707b92195fae0cff2c5f65f2db6555c64f6e04f8
Satyam Singh <iamstymsingh@gmail.com>
2025-02-13 10:29:20 +0530

    Adding database connection logic

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	branca "github.com/hako/branca"
+	"log"
+)
+
+
+const (
+	databaseURL = "postgresql://root@localhost:26257/defaultdb?sslmode=disable"
+	port = 26257
+)
 
 func main() {
 	fmt.Println("Welcome to Shangrila.")
+
+	// Open database connection
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		log.Fatalf("Could not open database connection :%v/n", err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Could not ping database :%v/n", err)
+	}
 }
